a4ca7dbb38b1d193239a1d7d1812c5ca5953c21b
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-07-15 13:49:10 -0700

    destructure array

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// destructure an array
+// assign variable name based on position in array
+const [firstCity, secondCity, thirdCity]= [
+  "New York",
+  "Tokyo",
+  "Berlin"
+]
+
+console.log(secondCity)
+
+//instead of passing props, destructure the array to give just the value you want
+function App({library}) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello from {library}.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
