505c9667570dc646c8e6f1bc33c40edfc35ad113
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-07-15 14:29:06 -0700

    set state variable

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 // destructure an array
 // assign variable name based on position in array
-const [firstCity, secondCity, thirdCity]= [
-  "New York",
-  "Tokyo",
-  "Berlin"
-]
+// const [firstCity, secondCity, thirdCity]= [
+//   "New York",
+//   "Tokyo",
+//   "Berlin"
+// ]
 
-console.log(secondCity)
+// console.log(secondCity)
 
 //instead of passing props, destructure the array to give just the value you want
-function App({library}) {
+function App() {
+  // set state value will be initial one when loaded
+  const [emotion, setEmotion]= useState("happy");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from {library}.
+          Current emotion is {emotion}
         </p>
+        <button onClick={() => setEmotion("sad")}>Sad</button>
       </header>
     </div>
   );
