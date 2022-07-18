9d5517c52e86009448fb6a8c41cac4cdd6e3d69a
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-07-18 15:21:11 -0700

    useEffect()

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 // destructure an array
 // assign variable name based on position in array
@@ -16,14 +16,29 @@ import {useState} from "react";
 function App() {
   // set state value will be initial one when loaded
   const [emotion, setEmotion]= useState("happy");
+
+  const [secondaryEmotion, setSecondaryEmotion] = useState("tired")
+
+  // useEffect happens alongside render
+  // if you want to be called only once, pass in dependency array - , []
+  // or pass in array with state value - [emotion] to call function when state updates
+  useEffect(() => {
+    console.log(`It's ${emotion} right now`)
+  }, [emotion])
+  useEffect(() =>{
+    console.log(`It's ${secondaryEmotion} around here`)
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Current emotion is {emotion}
-        </p>
+        </h1>
         <button onClick={() => setEmotion("sad")}>Sad</button>
+        <button onClick={() => setEmotion("excited")}>Excited</button>
+        <h2>Current secondary emotion is {secondaryEmotion}</h2>
+        <button onClick={() => setSecondaryEmotion("stressed")}>Stressed</button>
       </header>
     </div>
   );
